4aa9c7c06f9f398cb62ae0718d6a7cc272fc23f0
hOlrrDtr <danylo.shporta@nure.ua>
2023-10-06 23:32:44 +0300

    new tests

--- main.go
+++ main.go
@@ -9,48 +9,27 @@ import (
 )
 
 func main() {
-	curveECC := curves.Secp256k1
+	curveED := curves.Ed25519
 	k := 0
-	for j := 0; j < 100; j++ {
-		priv_key, pub_key := signature.Gen_keys(curveECC)
-		// fmt.Println("private key: ", priv_key)
-		// fmt.Println("public key:  ", curveECC.PointToString(pub_key))
+	for i := 0; i < 100; i++ {
+		_, pub_key := signature.Gen_keys(curveED)
+		priv_key, _ := signature.Gen_keys(curveED)
 		s := 3
-		n := 10
-		pub_keys := make([]point.Point, n)
-		for i := 0; i < n; i++ {
-			if i == s {
-				pub_keys[i] = pub_key
+		var ring []point.Point
+		for j := 0; j < 10; j++ {
+			if j == s {
+				ring = append(ring, pub_key)
 			} else {
-				_, pub_keys[i] = signature.Gen_keys(curveECC)
-				// fmt.Println("pub_key ", i, ": ", curveECC.PointToString(pub_keys[i]))
+				_, p_k := signature.Gen_keys(curveED)
+				ring = append(ring, p_k)
 			}
 		}
-		test_sig := signature.Sign(curveECC, "Hello", pub_keys, s, *priv_key)
-		// fmt.Println("Signing")
-		test := signature.Verify(test_sig)
-		// fmt.Println("Verify")
-		fmt.Println(test)
-		test = false
-		curveEd := curves.Ed25519
-		priv_key, pub_key = signature.Gen_keys(curveEd)
-		// fmt.Println("private key: ", priv_key)
-		// fmt.Println("public key:  ", curveEd.PointToString(pub_key))
-		for i := 0; i < n; i++ {
-			if i == s {
-				pub_keys[i] = pub_key
-			} else {
-				_, pub_keys[i] = signature.Gen_keys(curveEd)
-				// fmt.Println("pub_key ", i, ": ", curveEd.PointToString(pub_keys[i]))
-			}
-		}
-
-		test_sig = signature.Sign(curveEd, "Hello", pub_keys, s, *priv_key)
-		test = signature.Verify(test_sig)
-		if test == true {
+		message := "jklfdhgslkfhjlgskfdsbnstpivlwyreontbviw[brepo]69b0-54"
+		new_sig := signature.Sign(curveED, message, ring, s, *priv_key)
+		verif := signature.Verify(new_sig)
+		if !verif {
 			k++
 		}
-		fmt.Println(test)
 	}
 	fmt.Println(k)
 
--- signature_test/signature_test.go
+++ signature_test/signature_test.go
@@ -159,6 +159,144 @@ func TestVerify(t *testing.T) {
 		t.Errorf("must be 100 invalid signatures")
 	}
 
+	testFileTrueED, err := os.Open("test_ed25519_true.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer testFileTrueED.Close()
+	testFileFalseED, err := os.Open("test_ed25519_false.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer testFileFalseED.Close()
+	// //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	index = 0
+	scanner = bufio.NewScanner(testFileTrueED)
+	for scanner.Scan() {
+		Pub_keys = *new([]point.Point)
+		C = *new([]big.Int)
+		R = *new([]big.Int)
+		message = scanner.Text()
+		scanner.Scan()
+		curve = signature.StringToCurve(scanner.Text())
+		scanner.Scan()
+		n, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < n; i++ {
+			scanner.Scan()
+			Pub_keys = append(Pub_keys, curve.StringToPoint(scanner.Text()))
+		}
+		scanner.Scan()
+		I = curve.StringToPoint(scanner.Text())
+		scanner.Scan()
+		n, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < n; i++ {
+			scanner.Scan()
+			c_i, _ := big.NewInt(0).SetString(scanner.Text(), 0)
+			C = append(C, *c_i)
+		}
+		scanner.Scan()
+		n, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < n; i++ {
+			scanner.Scan()
+			r_i, _ := big.NewInt(0).SetString(scanner.Text(), 0)
+			R = append(R, *r_i)
+		}
+		new_sig := signature.TraceRingSignature{
+			Message:  message,
+			Curve:    curve,
+			Pub_keys: Pub_keys,
+			I:        I,
+			C:        C,
+			R:        R,
+		}
+		test_sigs[index] = new_sig
+		index++
+	}
+	k = 0
+	verif = false
+	for i := 0; i < 100; i++ {
+		verif = signature.Verify(test_sigs[i])
+		if !verif {
+			t.Errorf("all test signature must be valid")
+		}
+		k++
+	}
+	if k != 100 {
+		t.Errorf("must be 100 valid signatures")
+	}
+
+	scanner = bufio.NewScanner(testFileFalseED)
+	index = 0
+	for scanner.Scan() {
+		Pub_keys = *new([]point.Point)
+		C = *new([]big.Int)
+		R = *new([]big.Int)
+		message = scanner.Text()
+		scanner.Scan()
+		curve = signature.StringToCurve(scanner.Text())
+		scanner.Scan()
+		n, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < n; i++ {
+			scanner.Scan()
+			Pub_keys = append(Pub_keys, curve.StringToPoint(scanner.Text()))
+		}
+		scanner.Scan()
+		I = curve.StringToPoint(scanner.Text())
+		scanner.Scan()
+		n, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < n; i++ {
+			scanner.Scan()
+			c_i, _ := big.NewInt(0).SetString(scanner.Text(), 0)
+			C = append(C, *c_i)
+		}
+		scanner.Scan()
+		n, err = strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		for i := 0; i < n; i++ {
+			scanner.Scan()
+			r_i, _ := big.NewInt(0).SetString(scanner.Text(), 0)
+			R = append(R, *r_i)
+		}
+		new_sig := signature.TraceRingSignature{
+			Message:  message,
+			Curve:    curve,
+			Pub_keys: Pub_keys,
+			I:        I,
+			C:        C,
+			R:        R,
+		}
+		test_sigs[index] = new_sig
+		index++
+	}
+	k = 0
+	verif = false
+	for i := 0; i < 100; i++ {
+		verif = signature.Verify(test_sigs[i])
+		if verif {
+			t.Errorf("all test signature must be invalid")
+		}
+		k++
+	}
+	if k != 100 {
+		t.Errorf("must be 100 invalid signatures")
+	}
 }
 
 func TestLink(t *testing.T) {
@@ -183,7 +321,7 @@ func TestLink(t *testing.T) {
 	if err != nil {
 		log.Fatal(err)
 	}
-	testFile, err = os.Open("test_secp256k1_link.txt")
+	testFile, err = os.Open("test_link.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
