dca90bd311cb42a9d4c34bbd4bedb70afe676535
hOlrrDtr <danylo.shporta@nure.ua>
2023-10-09 12:41:03 +0300

    refactor main

--- main.go
+++ main.go
@@ -2,35 +2,44 @@ package main
 
 import (
 	"fmt"
-	// "math/big"
 	"trace_ring_sig/curves"
 	"trace_ring_sig/point"
 	"trace_ring_sig/signature"
 )
 
 func main() {
-	curveED := curves.Ed25519
-	k := 0
-	for i := 0; i < 100; i++ {
-		_, pub_key := signature.Gen_keys(curveED)
-		priv_key, _ := signature.Gen_keys(curveED)
-		s := 3
-		var ring []point.Point
-		for j := 0; j < 10; j++ {
-			if j == s {
-				ring = append(ring, pub_key)
-			} else {
-				_, p_k := signature.Gen_keys(curveED)
-				ring = append(ring, p_k)
-			}
-		}
-		message := "jklfdhgslkfhjlgskfdsbnstpivlwyreontbviw[brepo]69b0-54"
-		new_sig := signature.Sign(curveED, message, ring, s, *priv_key)
-		verif := signature.Verify(new_sig)
-		if !verif {
-			k++
+	curveSC := curves.Secp256k1
+	mess := "gfjkdjlk"
+	priv_key, pub_key := signature.Gen_keys(curveSC)
+	var (
+		ring     []point.Point
+		ring_key point.Point
+	)
+	s := 3
+	for i := 0; i < 10; i++ {
+		if i == s {
+			ring = append(ring, pub_key)
+		} else {
+			_, ring_key = signature.Gen_keys(curveSC)
+			ring = append(ring, ring_key)
 		}
 	}
-	fmt.Println(k)
+	scepcSig := signature.Sign(curveSC, mess, ring, s, *priv_key)
+	verif := false
+	verif = signature.Verify(scepcSig)
+	fmt.Println("Secp256k1:", verif)
+	curveED := curves.Ed25519
+	priv_key, pub_key = signature.Gen_keys(curveED)
+	for i := 0; i < 10; i++ {
+		if i != s {
+			_, ring_key = signature.Gen_keys(curveED)
+			ring[i] = ring_key
+		} else {
+			ring[i] = pub_key
+		}
 
+	}
+	sigED := signature.Sign(curveED, mess, ring, s, *priv_key)
+	verif = signature.Verify(sigED)
+	fmt.Println("Ed25519:", verif)
 }
